e009107235cdb1ce1b9e67ca0d7d48fb8cbfedb6
jdinsh <jdinsh@gmail.com>
2022-10-21 15:20:01 -0400

    Added configuration

--- src/main/java/com/wellsFargo/CommBo/configuration/CommandConfiguration.java
+++ src/main/java/com/wellsFargo/CommBo/configuration/CommandConfiguration.java
@@ -1,17 +1,37 @@
 package com.wellsFargo.CommBo.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CommandConfiguration {
 
+    @Getter
+    @Setter
+    private String[] allowedCors = {"http://localhost:4200/","http://localhost:8080/"};
     @Bean
     public WebClient commandWebClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder
                 .baseUrl("http://localhost:5005")
                 .build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/intent/postCommand").allowedOrigins(allowedCors);
+            }
+        };
+    }
+
 }
 
